b37e1c4dc63931c0c60e0338f1265c7b515eae4c
mountaincenter <y.hiroyuki560730@gmail.com>
2022-09-13 12:12:25 +0900

    create post_controller and routing

---
+++ backend/app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,36 @@
+class Api::V1::PostsController < ApplicationController
+  before_action :set_post, only: %i[show destroy]
+
+  def index
+    posts = Post.order(created_at: :desc).limit(20)
+    render json: posts
+  end
+
+  def show
+    render json: @post
+  end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      render json: post
+    else
+      render json: { errors: post.errors }
+    end
+  end
+
+  def destroy
+    @post.destroy
+    render json: @post
+  end
+
+  private
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.permit(:content, { images: []}, :user_id)
+    end
+end
